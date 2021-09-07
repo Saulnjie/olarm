a36b226d9548e55a6fb9ddc91a719460d9b4f111
spettenn <aleksanderspetalen@gmail.com>
2021-09-07 10:06:47 +0200

    hello

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,45 @@
 import React from 'react';
 
 function Navigation() {
-	return <div></div>;
+	return (
+		<div>
+			<>
+				<Navbar bg='light' variant='light'>
+					<Container>
+						<Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+						<Nav className='me-auto'>
+							<Nav.Link href='#home'>Home</Nav.Link>
+							<Nav.Link href='#features'>Features</Nav.Link>
+							<Nav.Link href='#pricing'>Pricing</Nav.Link>
+						</Nav>
+					</Container>
+				</Navbar>
+				<br />
+				<Navbar bg='primary' variant='light'>
+					<Container>
+						<Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+						<Nav className='me-auto'>
+							<Nav.Link href='#home'>Home</Nav.Link>
+							<Nav.Link href='#features'>Features</Nav.Link>
+							<Nav.Link href='#pricing'>Pricing</Nav.Link>
+						</Nav>
+					</Container>
+				</Navbar>
+
+				<br />
+				<Navbar bg='light' variant='light'>
+					<Container>
+						<Navbar.Brand href='#home'>Navbar</Navbar.Brand>
+						<Nav className='me-auto'>
+							<Nav.Link href='#home'>Home</Nav.Link>
+							<Nav.Link href='#features'>Features</Nav.Link>
+							<Nav.Link href='#pricing'>Pricing</Nav.Link>
+						</Nav>
+					</Container>
+				</Navbar>
+			</>
+		</div>
+	);
 }
 
 export default Navigation;
