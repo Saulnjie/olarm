0fe0e492adfbbc0cd18a98dc3428d725e5371b40
spettenn <69841778+spettenn@users.noreply.github.com>
2021-09-07 13:38:10 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 // import "./scss/styles";
 
-import './scss/styles.scss';
+import "./scss/styles.scss";
 
-import Col from 'react-bootstrap/Col';
+import Col from "react-bootstrap/Col";
 //CUSTOM COMPONENTS
-import Navigation from '../src/components/Navigation';
+import Navigation from "../src/components/Navigation";
+import Nav__Logo from "./components/Nav__Logo";
 
 function App() {
-	return (
-		<div class='container'>
-			<div class='col sm-10'>
-				<Navigation />
-			</div>
-		</div>
-	);
+  return (
+    <div class="container">
+      <div class="col-sm-3">
+        <Nav__Logo />
+      </div>
+      <div class="col sm-6">
+        <Navigation />
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+  );
 }
 
 export default App;
