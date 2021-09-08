6539ec5c934920c71981fdc2e46e4f186b64a7b3
SAUL_NJIE <SAULUS92@gmail.com>
2021-09-08 11:16:41 +0200

    page two layout

--- src/App.js
+++ src/App.js
@@ -13,36 +13,53 @@ import olarm_logo_image from "./images/olarm_logo.png";
 
 function App() {
   return (
-    <section
-      class="landing__page"
-      style={{ backgroundImage: `url(${backgroundLanding})` }}
-    >
-      <div className="container container__Nav">
-        <div className="col-sm-3">
-          <img src={olarm_logo_image} />
+    <div className="wrapper">
+      <section
+        class="landing__page"
+        style={{ backgroundImage: `url(${backgroundLanding})` }}
+      >
+        <div className="container container__Nav">
+          <div className="col-sm-3">
+            <img src={olarm_logo_image} />
+          </div>
+          <div className="col-sm-6">
+            <Navigation />
+          </div>
+          <div className="col-sm-3 navBtns">
+            <Buttons />
+          </div>
         </div>
-        <div className="col-sm-6">
-          <Navigation />
+        <div className="container landing__info">
+          <div className="col-sm-8">
+            <Information />
+          </div>
+          <div className="col-sm-2"></div>
+          <div className="col-sm-2"></div>
         </div>
-        <div className="col-sm-3 navBtns">
-          <Buttons />
+        <div className="container">
+          <div className="col-sm-8">
+            <CardsLanding />
+          </div>
+          <div className="col-sm-2"></div>
+          <div className="col-sm-2"></div>
         </div>
-      </div>
-      <div className="container landing__info">
-        <div className="col-sm-8">
-          <Information />
-        </div>
-        <div className="col-sm-2"></div>
-        <div className="col-sm-2"></div>
-      </div>
-      <div className="container">
-        <div className="col-sm-8">
-          <CardsLanding />
-        </div>
-        <div className="col-sm-2"></div>
-        <div className="col-sm-2"></div>
-      </div>
-    </section>
+        <section className="page_two">
+          <div className="Container page_two__sections">
+            <div className="col-sm-6">a</div>
+            <div className="col-sm-3">b</div>
+            <div className="col-sm-3">c</div>
+          </div>
+          <div className="Container page_two__sections">
+            <div className="col-sm-8">d</div>
+            <div className="col-sm-4">e</div>
+          </div>
+          <div className="Container page_two__sections">
+            <div className="col-sm-4">f</div>
+            <div className="col-sm-8">g</div>
+          </div>
+        </section>
+      </section>
+    </div>
   );
 }
 
